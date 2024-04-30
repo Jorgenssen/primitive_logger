3b48797fa3ee03b9e83da9509271472fa18acc21
Jorgenssen <n.tatyannikov@gmail.com>
2024-04-30 09:54:28 +0100

    Update logger.c

--- src/logger.c
+++ src/logger.c
@@ -1,7 +1,15 @@
+/**
+ * logger.c -- util wrapping logging methods
+ *
+ * Author: Nikita Tatiannikov 
+ * Written on Monday, 14 May 2021.
+ * Copyright (c) 2021
+ * https://github.com/Jorgenssen/primitive_logger/
+ */
+
 #include "logger.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>  // for printf
+#include <time.h>   // for localtime, strftime, time, time_t
 
 void logger(const char* tag, const char* message) {
   time_t now;
